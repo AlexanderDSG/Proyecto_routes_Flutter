a58e28511082c14d2d815d4a437fffb6003f3542
Alexander <alexandersuntaxi20030414@gmail.com>
2024-05-22 19:13:19 -0500

    Update parcial1

---
+++ lib/Pages/consumer_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ConsumerPage extends StatelessWidget {
+  const ConsumerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/Routes/router.dart
+++ lib/Routes/router.dart
@@ -2,13 +2,15 @@ import 'package:go_router/go_router.dart';
 import 'package:proyecto_routes_app/Pages/detail.dart';
 import 'package:proyecto_routes_app/Pages/list_prod.dart';
 import 'package:proyecto_routes_app/Pages/shop_card.dart';
-import 'package:proyecto_routes_app/main.dart'; 
+import 'package:proyecto_routes_app/main.dart';
+import 'package:proyecto_routes_app/Pages/consumer_page.dart'; 
 
 class RouteNames {
   static String home = '/';
   static String detalles= '/detalles';
   static String listaProducto = '/ListaProd';
   static String tienda = '/tienda';
+  static String consumer = '/consumer';
 }
 final router = GoRouter(routes:[
   GoRoute(
@@ -26,5 +28,9 @@ final router = GoRouter(routes:[
   GoRoute(
     path: RouteNames.tienda,
     builder: (context, state) =>const ShopCard()
+  ),
+  GoRoute(
+    path: RouteNames.consumer,
+    builder: (context, state) =>const ConsumerPage()
   ),
 ]);
